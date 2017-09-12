0cdbd01aca8028536a02e428de0011c1127b6bc3
srihithalwala <srihithalwala@gmail.com>
2017-09-12 15:35:23 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		int a=4,b=5;
-		return a+b;
+		System.out.println(a+b);
 		//You may test that your code works find here
 		//Please check that your code works and has no 
 		//compilation problems before to submit
