a6fe33fce1c4a5b2edab0cd16b2a10e71a81bf4f
srihithalwala <srihithalwala@gmail.com>
2017-09-12 15:34:18 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) {
+		int a=4,b=5;
+		return a+b;
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
